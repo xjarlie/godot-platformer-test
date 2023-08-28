9fe2f8d01707debcbf98236ad87d59b2793185d4
Charlie Appleton <xjarlie@gmail.com>
2023-08-28 20:37:40 +0900

    Update player.gd

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 @export var SPEED = 300.0
-@export var JUMP_SPEED = 800.0
+@export var JUMP_SPEED = 900.0
 @export_range(0.0, 1.0) var friction = 0.1;
 @export_range(0.0, 1.0) var acceleration = 0.25;
 @export var gravity = 3000;
@@ -29,5 +29,11 @@ func _physics_process(delta):
 	else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.x = lerp(velocity.x, 0.0, friction)
+		
+	if velocity.x > 0:
+		$Sprite2D.set_flip_h(false);
+	elif velocity.x < 0:
+		$Sprite2D.set_flip_h(true);
+		
 
 	move_and_slide()
