e46cc25f626c675cf0f54983bb89311d45beb1c9
Charlie Appleton <xjarlie@gmail.com>
2023-08-31 22:11:03 +0900

    ui

--- main.gd
+++ main.gd
@@ -13,15 +13,20 @@ func begin():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_action_just_pressed("pause")):
-		$PauseMenu.show();
-		get_tree().paused = true;
+		pause_game()
 		
 func _physics_process(delta):
 	
 	if ($Player.is_on_floor()):
 		$Spirit.regen_stamina(2);
 
-
+func pause_game():
+	$PauseMenu.show();
+	get_tree().paused = true;
 
 func _on_pause_menu_restart():
 	begin();
+
+
+func _on_ui_pause():
+	pause_game();
